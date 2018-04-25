e764ca7622cd96ac00ae2f3190e2ff686962e39c
Salvatore Ravidà <salvoravida@gmail.com>
2018-04-25 02:38:24 +0200

    withAdalLogin HOC

--- src/react-adal.js
+++ src/react-adal.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line
+import React from 'react';
 import AuthenticationContext_ from './adal';
 
 export const AuthenticationContext = AuthenticationContext_;
@@ -12,7 +14,7 @@ export function adalGetToken(authContext, resourceGuiId) {
   });
 }
 
-export function runWithAdal(authContext, app) {
+export function runWithAdal(authContext, app, doNotLogin) {
   //it must run in iframe to for refreshToken (parsing hash and get token)
   authContext.handleWindowCallback();
 
@@ -21,7 +23,11 @@ export function runWithAdal(authContext, app) {
     if (!authContext.isCallback(window.location.hash)) {
       if (!authContext.getCachedToken(authContext.config.clientId) ||
           !authContext.getCachedUser()) {
-        authContext.login();
+        if (doNotLogin) {
+          app();
+        } else {
+          authContext.login();
+        }
       } else {
         app();
       }
@@ -31,9 +37,45 @@ export function runWithAdal(authContext, app) {
 
 export function adalFetch(authContext, resourceGuiId, fetch, url, options) {
   return adalGetToken(authContext, resourceGuiId).then((token) => {
-    const o = options;
+    const o = options || {};
     if (!o.headers) o.headers = {};
     o.headers.Authorization = `Bearer ${token}`;
     return fetch(url, options);
   });
 }
+
+// eslint-disable-next-line
+export const withAdalLogin = (authContext, resourceId) => {
+  // eslint-disable-next-line
+  return function(WrappedComponent, renderLoading, renderError) {
+    return class extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          logged: false,
+          error: null,
+        };
+      }
+
+      componentWillMount = () => {
+        adalGetToken(authContext, resourceId)
+          .then(() => this.setState({ logged: true }))
+          .catch((error) => {
+            const { msg } = error;
+            console.log(error);
+            if (msg === 'login required') {
+              authContext.login();
+            } else {
+              this.setState({ error });
+            }
+          });
+      };
+
+      render() {
+        if (this.state.logged) return <WrappedComponent {...this.props} />;
+        if (this.state.error) return typeof renderError === 'function' ? renderError(this.state.error) : null;
+        return typeof renderLoading === 'function' ? renderLoading() : null;
+      }
+    };
+  };
+};
