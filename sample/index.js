02dbe465e03f067d0ebbf8c05dc0570e0ac17aec
blackfenix2 <devin_98@msn.com>
2019-07-29 19:44:48 -0400

    added parcel bundler for sample dev server

--- sample/index.js
+++ sample/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import App from './App';
-import { runWithAdal } from './react-adal';
+import { runWithAdal } from '../src/react-adal';
 import { authContext } from './adalConfig';
 
-runWithAdal(authContext, () => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('root'),
-  );
-});
+const App = () => (
+  <div>
+    <p>Test App with Login</p>
+  </div>
+);
 
+runWithAdal(authContext, () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+});
