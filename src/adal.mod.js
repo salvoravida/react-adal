36e462cdd4269d13fa5adec0f8ce2e2bba4e3bd0
Maciej Kankowski <mackankowski@gmail.com>
2020-12-31 15:40:35 +0100

    Fix UPN for Typescript 4

--- src/adal.mod.js
+++ src/adal.mod.js
@@ -4,29 +4,40 @@ import AuthenticationContext from './adal';
  * Validates each resource token in cache againt current user
  */
 AuthenticationContext.prototype.invalidateResourceTokens = function () {
-  if (!this.config.endpoints) { return; }
+  if (!this.config.endpoints) {
+    return;
+  }
   const idToken = this._getItem(this.CONSTANTS.STORAGE.IDTOKEN);
-  if (!idToken) { return; }
+  if (!idToken) {
+    return;
+  }
   const { upn } = this._extractIdToken(idToken);
   const resources = Object.values(this.config.endpoints);
 
-  resources.forEach(r => this._clearStaleResourceToken(r, upn));
-}
+  resources.forEach((r) => this._clearStaleResourceToken(r, upn));
+};
 
 /**
  * Clears cache for the given resource if it doesn't belong to current user's UPN
  * @param {string} currentUserUpn Unique user identifier
  * @param {string} resource a URI that identifies the resource
  */
-AuthenticationContext.prototype._clearStaleResourceToken = function (resource, currentUserUpn) {
+AuthenticationContext.prototype._clearStaleResourceToken = function (
+  resource,
+  currentUserUpn
+) {
   const resourceToken = this.getCachedToken(resource);
   if (resourceToken) {
     const { upn } = this._extractIdToken(resourceToken);
-    if (upn.toLowerCase() !== currentUserUpn.toLowerCase()) {
+    if (
+      typeof upn == 'string' &&
+      typeof currentUserUpn == 'string' &&
+      upn.toLowerCase() !== currentUserUpn.toLowerCase()
+    ) {
       this.info(`Clearing invalid cache of resource ${resource}`);
       this.clearCacheForResource(resource);
     }
   }
-}
+};
 
-export default AuthenticationContext
+export default AuthenticationContext;
